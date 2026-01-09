75186863a95eb396b255f6607df7297b0a5c83c2
Sagar_Yadav <yadavfamilyinfo03@gmail.com>
2026-01-09 19:42:26 +0545

    Create initial React component App

---
+++ Apps.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Hello World 👋</h1>
+      <p>My first React App</p>
+    </div>
+  );
+}
+
+export default App;
